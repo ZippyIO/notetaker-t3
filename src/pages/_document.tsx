306ac80c1237681f2d6eab3be88ec92897391173
Nick <delta2888@gmail.com>
2023-04-12 14:49:02 +1000

    ✨ feat: add document layout

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document = () => {
+    return (
+        <Html data-theme="winter">
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+};
+export default Document;
